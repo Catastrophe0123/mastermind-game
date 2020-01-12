358cc6903ccd395de3684ad3a2051f6eb1ab55d8
Catastrophe0123 <vnavaneeth15@gmail.com>
2020-01-12 18:38:23 +0530

    Updates

---
+++ precache-manifest.7c9792a2d2cf962afab984addc800d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9160abf22de937daea2b65e14e67e1e1",
+    "url": "/mastermind-game/index.html"
+  },
+  {
+    "revision": "14ef28ef649a855e5405",
+    "url": "/mastermind-game/static/css/main.4bc72368.chunk.css"
+  },
+  {
+    "revision": "5bcdb11fd7881f2ce30b",
+    "url": "/mastermind-game/static/js/2.c1c37226.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mastermind-game/static/js/2.c1c37226.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14ef28ef649a855e5405",
+    "url": "/mastermind-game/static/js/main.c400e49a.chunk.js"
+  },
+  {
+    "revision": "24f6a0de3071a8705e1d",
+    "url": "/mastermind-game/static/js/runtime-main.8808dee7.js"
+  }
+]);
